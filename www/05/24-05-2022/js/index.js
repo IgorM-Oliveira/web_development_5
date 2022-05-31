99e92e4467f1098781f65ab303d378901a95640e
Laboratorio <laboratorio@lab.unigran>
2022-05-30 20:23:10 -0400

    30-05-2022

--- www/05/24-05-2022/js/index.js
+++ www/05/24-05-2022/js/index.js
@@ -1,4 +1,22 @@
 function enviar() {
-    const data = document.getElementsById('name')
-    console.log(data)
+    const name = document.getElementById('name').value
+    const phone = document.getElementById('phone').value
+    const email = document.getElementById('email').value
+    const cep = document.getElementById('cep').value
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`).then((response) => {
+        response.json()
+        .then(data => {
+            document.getElementById('form_input').style.display = 'none';
+            document.getElementById('form_value').style.display = 'flex';
+            document.getElementById('bairro').innerHTML = data.bairro
+            document.getElementById('local').innerHTML = data.localidade
+            document.getElementById('logra').innerHTML = data.logradouro
+            document.getElementById('uf').innerHTML = data.uf
+        })
+      }).catch((error) => {
+          console.log(error)
+      })
+
+    console.log(name, phone, email, cep)
 }
