a471866235bb637f655d222943601a59a57f70b6
Laboratorio <laboratorio@lab.unigran>
2022-05-31 21:54:14 -0400

    31-05

--- www/05/24-05-2022/js/index.js
+++ www/05/24-05-2022/js/index.js
@@ -9,6 +9,14 @@ function enviar() {
         .then(data => {
             document.getElementById('form_input').style.display = 'none';
             document.getElementById('form_value').style.display = 'flex';
+
+            var table = document.getElementById("myTable");
+            var row = table.insertRow(0);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            cell1.innerHTML = "NEW CELL1";
+            cell2.innerHTML = "NEW CELL2";
+
             document.getElementById('bairro').innerHTML = data.bairro
             document.getElementById('local').innerHTML = data.localidade
             document.getElementById('logra').innerHTML = data.logradouro
